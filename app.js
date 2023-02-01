a87ac31903e6ee6c8dd2917a20ff227130427249
ismetcanbyk <ismetcanbyk@gmail.com>
2023-02-01 23:29:21 +0300

    Add static file

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 import express from 'express';
+import ejs from 'ejs';
 
 const port = 3000;
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('merhaba');
-  res.end();
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
 app.listen(port, () => {
